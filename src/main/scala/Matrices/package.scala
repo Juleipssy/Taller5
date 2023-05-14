e1919743c48d395047549240c32dd094d3f668f7
Juleipssy <juleipssy.cely@correounivalle.edu.co>
2023-05-14 16:16:08 -0500

    PUNTOS ALGUNOS CUESTIONABLES

--- src/main/scala/Matrices/package.scala
+++ src/main/scala/Matrices/package.scala
@@ -8,34 +8,35 @@
   import scala.util.Random
 
 package object Matrices {
-    val random = new Random ( )
+  val random = new Random()
 
-    type Matriz = Vector [ Vector [ Int ] ]
-    def matrizAlAzar ( long :Int , vals : Int ) : Matriz = {
-      //Crea una matriz de enteros cuadrada de long x long ,
-      // con valores aleatorios entre 0 y vals
-      val v = Vector . fill ( long , long ) {
-        random .nextInt ( vals )
-      }
-      v
+  type Matriz = Vector[Vector[Int]]
+
+  def matrizAlAzar(long: Int, vals: Int): Matriz = {
+    //Crea una matriz de enteros cuadrada de long x long ,
+    // con valores aleatorios entre 0 y vals
+    val v = Vector.fill(long, long) {
+      random.nextInt(vals)
     }
+    v
+  }
 
   /////////////////////////////////////////////////////////////////////////////////////
 
-    def transpuesta (m: Matriz ) : Matriz = {
-      val l=m.length
-        Vector . tabulate ( l , l ) ( ( i , j )=>m( j ) ( i ) )
-    }
+  def transpuesta(m: Matriz): Matriz = {
+    val l = m.length
+    Vector.tabulate(l, l)((i, j) => m(j)(i))
+  }
 
   ///////////////////////////////////////////////////////////////////////////////////////
 
-    def prodPunto ( v1 : Vector [ Int ] , v2 : Vector [ Int ] ) : Int ={
-      ( v1 zip v2 ) .map({ case ( i , j )=> ( i * j ) } ).sum
-    }
+  def prodPunto(v1: Vector[Int], v2: Vector[Int]): Int = {
+    (v1 zip v2).map({ case (i, j) => (i * j) }).sum
+  }
 
-///////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////
 
-    // Ejercicio 1.1.1
+  // Ejercicio 1.1.1
 
   def multMatriz(m1: Matriz, m2: Matriz): Matriz = {
     val l = m1.length
@@ -61,13 +62,14 @@ package object Matrices {
 
 
 
-    ////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////
 
-    // Ejercicio 1.1.2 ***********HAY ERROR******
+  // Ejercicio 1.1.2 ***********HAY ERROR******
 
 
-  def multMatrizPar (m1 : Matriz , m2 : Matriz ) : Matriz = {
+  def multMatrizPar(m1: Matriz, m2: Matriz): Matriz = {
     val l = m1.length
+
     val C = Vector.tabulate(l, l)((i, j) => {
       (0 until l).par.map(k => m1(i)(k) * m2(k)(j)).sum
     })
@@ -75,47 +77,61 @@ package object Matrices {
 
   }
 
+  def multMatrizPar(m1: Matriz, m2: Matriz): Matriz = {
+    val l1 = m1.length
+    val l2 = m2.length
+    val m2T = transpuesta(m2)
+
+    if (l1 >= 4) {
+      val mid = l1 / 2
+      val (m1R, m1L) = m1 splitAt (mid)
+      val (r1, r2) = parallel(multMatrizPar(m1R, m2), multMatrizPar(m1L, m2))
+      r1 ++ r2
+    }
+    else {
+      Vector.tabulate(l1, l2)((i, j) => prodPunto(m1(i), m2T(j)))
+    }
+  }
 
 
 
-    ////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////
 
-    // Ejercicio 1.2.1   ********DUDAS SOBRE .MAP ***************************
+  // Ejercicio 1.2.1   ********DUDAS SOBRE .MAP ***************************
 
 
-
-  def subMatriz (m: Matriz , i : Int , j : Int , l : Int ) : Matriz ={
+  def subMatriz(m: Matriz, i: Int, j: Int, l: Int): Matriz = {
     val submatriz = (0 until l).map(a => (0 until l).map(c => m(i + a)(j + c)).toVector).toVector
 
     submatriz
   }
 
 
-  def subMatriz (m: Matriz , i : Int , j : Int , l : Int ) : Matriz ={
-  val submatriz = for {
-    a <- i until (i + l)
-    b = for {
-      c <- j until (j + l)
-    } yield m(a)(c)
-  } yield b.toVector
+  def subMatriz(m: Matriz, i: Int, j: Int, l: Int): Matriz = {
+    val submatriz = for {
+      a <- i until (i + l)
+      b = for {
+        c <- j until (j + l)
+      } yield m(a)(c)
+    } yield b.toVector
 
-  submatriz.toVector
-}
+    submatriz.toVector
+  }
 
   def subMatriz(m: Matriz, i: Int, j: Int, l: Int): Matriz = {
-    Vector.tabulate(l, l)((x, y) => m(i+x)(y+j))
+    Vector.tabulate(l, l)((x, y) => m(i + x)(y + j))
   }
 
 
-    //////////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////////
 
-    // Ejercicio 1.2.2
+  // Ejercicio 1.2.2
 
-  def sumMatriz (m1 : Matriz , m2 : Matriz ) : Matriz ={
-     val l = m1.length
-      val resultado = (0 until l).map(i => (0 until l).map(j => m1(i)(j) + m2(i)(j)).toVector).toVector
-      resultado
-    }
+  def sumMatriz(m1: Matriz, m2: Matriz): Matriz = {
+    val l = m1.length
+    val resultado = (0 until l).map(i => (0 until l).map(j => m1(i)(j) + m2(i)(j)).toVector).toVector
+    resultado
+  }
 
   def sumMatriz(m1: Matriz, m2: Matriz): Matriz = {
     val l = m1.length
@@ -123,35 +139,37 @@ package object Matrices {
   }
 
 
-  def sumMatriz (m1 : Matriz , m2 : Matriz ) : Matriz ={
-     val l = m1.length
+  def sumMatriz(m1: Matriz, m2: Matriz): Matriz = {
+    val l = m1.length
 
-      val resultado = for (i <- 0 until l) yield {
-        for (j <- 0 until l) yield {
-          m1(i)(j) + m2(i)(j)
-        }
+    val resultado = for (i <- 0 until l) yield {
+      for (j <- 0 until l) yield {
+        m1(i)(j) + m2(i)(j)
       }
-      resultado.map(_.toVector).toVector
     }
+    resultado.map(_.toVector).toVector
+  }
 
 
 
-    ////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    // Ejercicio 1.2.3 ********* TENGO MIS DUDAS **********
+  // Ejercicio 1.2.3 ********* TENGO MIS DUDAS **********
 
-    def multMatrizRec (m1 : Matriz , m2 : Matriz ) : Matriz ={
-       }
-  def multMatrizRec (m1 : Matriz , m2 : Matriz ) : Matriz ={
+
+  def multMatrizRec(m1: Matriz, m2: Matriz): Matriz = {
+
     def unirMatriz(m1: Matriz, m2: Matriz): Matriz = {
       val l = m1.length
       Vector.tabulate(l)((i) => m1(i) ++ m2(i))
     }
+
     val n = m1.length
     if (n == 1) {
       Vector(Vector(m1(0)(0) * m2(0)(0)))
     } else {
+
       val medio = n / 2
 
       val a11 = subMatriz(m1, 0, 0, medio)
@@ -179,39 +197,39 @@ package object Matrices {
 
 
 
-    ////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    // Ejercicio 1.2.4
-    def multMatrizRecPar (m1 : Matriz , m2 : Matriz ) : Matriz ={
-      def unirMatriz(m1: Matriz, m2: Matriz): Matriz = {
-        val l = m1.length
-        Vector.tabulate(l)((i) => m1(i) ++ m2(i))
-      }
-      }
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-
-    // Ejercicio 1.3.1
-    def restaMatriz(m1: Matriz, m2: Matriz): Matriz={
-
-    }
+  // Ejercicio 1.2.4 *****************FALTA COLOCARLO*********************
+  def multMatrizRecPar(m1: Matriz, m2: Matriz): Matriz = {
+  }
 
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // Ejercicio 1.3.2
-    def multStrassen(m1: Matriz, m2: Matriz): Matriz = {
+  ///////////////////////////////////////////////////////////////////////////////////////////////
 
-    }
+  // Ejercicio 1.3.1
+  def restaMatriz(m1: Matriz, m2: Matriz): Matriz = {
+    val l = m1.length
+    Vector.tabulate(l, l)((i, j) => m1(i)(j) - m2(i)(j))
+  }
 
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // Ejercicio 1.3.3
-    def multStrassenPar(m1: Matriz, m2: Matriz): Matriz = {
+  // Ejercicio 1.3.2
+  def multStrassen(m1: Matriz, m2: Matriz): Matriz = {
 
-    }
+  }
+
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // Ejercicio 1.3.3
+  def multStrassenPar(m1: Matriz, m2: Matriz): Matriz = {
+
+  }
+
 
 
 }
